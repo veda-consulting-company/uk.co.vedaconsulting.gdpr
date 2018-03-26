dc6660d57e2d18c86ba11cbb245fb13694a8ca9a
Che Molava <che@vedaconsulting.co.uk>
2018-03-26 17:33:10 +0100

    Validate completion redirect url.

--- CRM/Gdpr/Form/CommunicationsPreferences.php
+++ CRM/Gdpr/Form/CommunicationsPreferences.php
@@ -245,6 +245,58 @@ class CRM_Gdpr_Form_CommunicationsPreferences extends CRM_Core_Form {
   protected function getProfileOptions() {
 
   }
+  public function addRules() {
+    $this->addFormRule(array('CRM_Gdpr_Form_CommunicationsPreferences', 'validateRedirectUrl'));
+  }
+
+  /**
+   * Validation callback for completion redirect url.
+   */
+  public function validateRedirectUrl($values) {
+    $errors = array();
+    if (!empty($values['completion_redirect'])) {
+      if (empty($values['completion_url'])) {
+        $errors['completion_url'] = ts('Please add the URL of a page to redirect the user after they complete the form.');
+      }
+      else {
+        $url = $values['completion_url'];
+        $parsed_url = parse_url($url);
+        $base_url = CIVICRM_UF_BASEURL;
+        if (!empty($parsed_url['host']) && !empty($parsed_url['scheme'])) {
+          $full_url = $url;
+        }
+        else {
+          if (0 === strpos($url, '/')) {
+            $url = substr($url, 1);
+          }
+          $full_url =  CIVICRM_UF_BASEURL . $url;
+        }
+
+        // We have been unable to construct a URL.
+        if (!$full_url) {
+          $errors['completion_url'] = ts('Invalid URL.');
+        }
+        elseif (function_exists('curl_init')) {
+          // Test if the url exists.
+          $ch  = curl_init();
+          curl_setopt($ch, CURLOPT_URL, $full_url);
+          curl_setopt($ch, CURLOPT_HEADER, TRUE);
+          curl_setopt($ch, CURLOPT_NOBODY, TRUE);
+          curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+          curl_setopt($ch, CURLOPT_FOLLOWLOCATION ,true);
+          curl_setopt($ch, CURLOPT_MAXREDIRS, 10);
+          $result = curl_exec($ch);
+          $code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+          $code = trim($code);
+          curl_close($ch);
+          if ($code[0] != '2') {
+            $errors['completion_url'] = ts('The completion URL does not belong to a valid page. Please check that an anonymous in user can access it.');
+          }
+        }
+      }
+    }
+    return $errors;
+  }
 
   /**
    * Get the fields/elements defined in this form.
